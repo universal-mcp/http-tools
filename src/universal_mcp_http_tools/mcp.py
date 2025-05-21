64a9bdf26b12fa182934341718338ece2f2544bf
Manoj Bajaj <manojbajaj95@gmail.com>
2025-05-21 23:28:12 +0530

    feat: Initial commit from MCP CLI init command

---
+++ src/universal_mcp_http_tools/mcp.py
@@ -0,0 +1,16 @@
+
+from universal_mcp.servers import SingleMCPServer
+
+from universal_mcp_http_tools.app import HttpToolsApp
+
+app_instance = HttpToolsApp()
+
+mcp = SingleMCPServer(
+    app_instance=app_instance
+)
+
+if __name__ == "__main__":
+    print(f"Starting {mcp.name}...")
+    mcp.run()
+
+
